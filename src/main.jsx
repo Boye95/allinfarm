f68eff04697ce6377124060a261ec2c62507e797
Desmond <desezo@gmail.com>
2023-12-17 13:49:11 +0000

    update for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+
 import Signup from "./pages/auth/signup";
 import Signin from "./pages/auth/signin";
-
+import NotFoundPage from "./pages/NotFoundPage"; 
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,10 +14,15 @@ const router = createBrowserRouter([
   {
     path: "/signin",
     element: <Signin />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
